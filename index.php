c4902cbccad4afc393689d27853d725cebc26ec6
zero4897hn <zero4897hncool@gmail.com>
2020-05-20 21:53:55 +0700

    Fixing pagination

--- index.php
+++ index.php
@@ -9,29 +9,29 @@
     <?php
         $db = new PDO('mysql:dbname=task_management;host:127.0.0.1;port=3306;charset=utf8mb4', 'root', 'nobita12');
 
-        $page = 0;
+        $page = 1;
 
-        $limit = 5;
+        $limit = 10;
 
         if (isset($_REQUEST['page'])) {
             $page = $_REQUEST['page'];
         }
 
-        if ($page < 0) {
-            $page = 0;
+        if ($page < 1) {
+            $page = 1;
         }
 
         $totalSize = $db->query('select count(1) as count from task')->fetchColumn();
 
-        $totalPage = ceil((int) $totalSize / $limit);
+        $totalPage = ceil(((int) $totalSize) / $limit);
 
-        if ($page > $totalPage - 1) {
-            $page = $totalPage - 1;
+        if ($page > $totalPage) {
+            $page = $totalPage;
         }
 
-        $statement = $db->prepare('select * from task limit ? offset ?');
+        $statement = $db->prepare('select * from task order by id desc limit ? offset ?');
         $statement->bindValue(1, (int) $limit, PDO::PARAM_INT);
-        $statement->bindValue(2, (int) ($page * $limit), PDO::PARAM_INT);
+        $statement->bindValue(2, (int) (($page  - 1) * $limit), PDO::PARAM_INT);
         $statement->execute();
     ?>
     <table>
@@ -51,7 +51,7 @@
                 while ($record = $statement->fetch()) {
             ?>
                 <tr>
-                    <td><?php echo $index + 1 + ($page * $limit) ?></td>
+                    <td><?php echo $index + 1 + (($page - 1) * $limit) ?></td>
                     <td><?php echo $record['name'] ?></td>
                     <td><?php echo $record['description'] ?></td>
                     <td><?php echo $record['created_at'] ?></td>
@@ -67,7 +67,9 @@
             ?>
         </tbody>
     </table>
-    <a href=".?page=<?php echo $page - 1; ?>"> << </a> <?php echo $page + 1; ?> <a href=".?page=<?php echo $page + 1; ?>"> >> </a><br>
+    <a onclick="if (<?php echo $page - 1; ?> < 1) return false;" href=".?page=<?php echo $page - 1; ?>"> << </a>
+    <?php echo $page; ?>
+    <a onclick="if (<?php echo $page + 1; ?> > <?php echo $totalPage; ?>) return false;" href=".?page=<?php echo $page + 1; ?>"> >> </a><br>
     <a href="add_task.php">Thêm mới tác vụ</a>
 </body>
 </html>
